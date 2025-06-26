091aefa6b904b8e64ae64c1e5e9b470775bcc4d4
Luciano Carril Reyes <luciano.carril@usach.cl>
2025-06-26 15:48:12 -0400

    envio el primer RF sin comprobaciones

---
+++ Jugador.java
@@ -0,0 +1,53 @@
+public class Jugador{
+    //atributos
+    private int id;
+    private String nombre;
+    private int dinero;
+    private List<propiedad> propiedades;
+    private int posicionActual;
+    private boolean estaEnCarcel;
+    private int totalCartasSalirCarcel;
+    //fin atributos
+    //constructor
+    public Jugador(int id,String nombre,int dinero, List<propiedad> propiedades, int posicionActual, boolean estaEnCarcel, int totalCartasSalirCarcel){
+        this.id=id;
+        this.nombre=nombre;
+        this.dinero=dinero;
+        this.propiedades=propiedades;
+        this.posicionActual=posicionActual;
+        this.estaEnCarcel=estaEnCarcel;
+        this.totalCartasSalirCarcel=totalCartasSalirCarcel;
+    }
+    //fin constructor
+//METODOS
+    //inicio de getters de todo
+    public int getId(){
+        return id;
+    }
+    //tal vez este no es necesario si no lo usas eliminalo
+    public String getNombre(){
+        return nombre;
+    }
+    public int getDinero(){
+        return dinero;
+    }
+    public List<propiedad> getPropiedades(){
+        return propiedades;
+    }
+    public int getPosicionActual(){
+        return posicionActual;
+    }
+    public boolean getEstaEnCarcel(){
+        return estaEnCarcel;
+    }
+    public int getTotalCartasSalirCarcel(){
+        return totalCartasSalirCarcel;
+    }
+
+//fin metodos
+
+//inico setter
+
+
+//fin setter
+}
